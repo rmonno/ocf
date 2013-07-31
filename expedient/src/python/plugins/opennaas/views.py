a77e3ad9170d19b34a67541010fc1aae59334cc2
Roberto Monno <r.monno@nextworks.it>
2013-07-31 16:48:23 +0200

    Add (initial) template file

--- expedient/src/python/plugins/opennaas/views.py
+++ expedient/src/python/plugins/opennaas/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponseServerError
+from django.core.urlresolvers import reverse
+from expedient.common.utils.views import generic_crud
+
+from opennaas.models import OpennaasAggregate
 
 import logging
 logger = logging.getLogger("sshaggregate-views")
 
 
 def aggregate_crud(request, agg_id=None):
-    logger.error("Not implemented!")
+    logger.debug("Aggregation Identifier=%s" % (agg_id,))
+    return generic_crud(request,
+                        obj_id=agg_id,
+                        model=OpennaasAggregate,
+                        template_object_name="aggregate",
+                        template="default/aggregate_crud.html",
+                        redirect=lambda inst: reverse(
+                            aggregate_add_resources, args=[inst.id]),
+                        success_msg=lambda inst: \
+                            "Successfully created/updated OpenNaaS Aggregate %s" % inst.name,)
+
+
+def aggregate_add_resources(request, agg_id):
     return HttpResponseServerError("Not implemented yet!")
 
 
