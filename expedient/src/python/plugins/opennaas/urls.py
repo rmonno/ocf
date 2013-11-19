21c1141497531322fbf617fb7343df71322c7e4b
Roberto Monno <r.monno@nextworks.it>
2013-11-19 14:14:16 +0100

    Introduced experient opennaas plugin in ofelia-development branch

---
+++ expedient/src/python/plugins/opennaas/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('opennaas.views',
+    url(r'^aggregate/create/$', 'aggregate_crud', name='opennaas_aggregate_create'),
+    url(r'^aggregate/(?P<agg_id>\d+)/edit/$', 'aggregate_crud', name='opennaas_aggregate_edit'),
+    url(r'^aggregate/(?P<agg_id>\d+)/list_resources/$', 'list_resources', name='list_resources'),
+    url(r'^aggregate/(?P<agg_id>\d+)/list_labels/(?P<name>\w+)/(?P<type>\w+)/(?P<endpoint>[0-9\-]+)/$', 'list_labels', name='list_labels'),
+
+    url(r'opennaas/describe/(?P<slice_id>\d+)/(?P<agg_id>\d+)/$', 'describe', name='describe'),
+    url(r'^opennaas/allocate/(?P<slice_id>\d+)/(?P<agg_id>\d+)/$', 'allocate', name='allocate'),
+    url(r'^opennaas/delete/(?P<slice_id>\d+)/(?P<agg_id>\d+)/$', 'delete', name='delete'),
+    url(r'^opennaas/renew/$', 'renew', name='renew'),
+)
